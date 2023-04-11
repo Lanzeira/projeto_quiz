a9d8c89740b9b4ae87b16f8f81fe965db15d3ee2
Suporte - Alex <101653857+Lanzeira@users.noreply.github.com>
2023-04-11 09:30:50 -0300

    Atualizei projeto - quiz

--- js/game.js
+++ js/game.js
@@ -8,7 +8,7 @@ const perguntas = [
     {
         pergunta: "Qual dessas linguagens não é considerado liguagem de programaçao?",
         respostas: ["PHP", "Javascript", "C++", "HTML"],
-        correta: "resp3"
+        correta: "resp2"
     },
     // PERGUNTA 1
     {
@@ -36,14 +36,14 @@ gerarPergunta(qtdPerguntas);
 
 function gerarPergunta(maxPerguntas) {
      //GERAR UM NUMERO ALEATORIO
-     let aleatorio = (Math.random() * maxPerguntas).toFixed();
+    let aleatorio = (Math.random() * maxPerguntas).toFixed();
      // CONVERTER PARA NUMERO
      aleatorio = Number(aleatorio);
      // MOSTRAR NO CONSOLE QUAL FOI A PERGUNTA SORTEADO
      console.log('A pergunta sorteado foi a:' + aleatorio);
 
      // VERIFICAR SE A PERGUNTA SORTEADA Jà FOI FEITA
-     if(!perguntasFeitas.includes(aleatoria)) {
+    if(!perguntasFeitas.includes(aleatorio)) {
         // COLOCAR COMO PERGUNTA FEITA
         perguntasFeitas.push(aleatorio);
 
@@ -51,9 +51,48 @@ function gerarPergunta(maxPerguntas) {
         var p_selecionada = perguntas[aleatorio].pergunta;
         console.log(p_selecionada);
         
-     }
+        //ALIMENTAR A PERGUNTA VINDA DO SORTEIO
+        $("#pergunta").html(p_selecionada);
+        $("#pergunta").attr('data-indice',aleatorio);
 
+        //COLOCAR AS RESPOSTAS
+        for (var i = 0;i < 4; i++) {
+            $("#resp" + i).html(perguntas[aleatorio].respostas[i]);
+        }
+        //EMBARALHAR AS RESPOSTAS
+        var pai = $("#respostas");
+        var botoes = pai.children();
+        
+        for (var i = 1; i < botoes.length; i++) {
+            pai.append(botoes.eq(Math.floor(Math.random()* botoes.length)));
+
+        }
+        
+    }
 }
+$('.resposta').click(function () {
+    $(this).addClass('selecionada');
+});
+
+$("#confirm").click(function () {
+    //PEGAR O INDICE DA PERGUNTA
+    var indice = $("#pergunta").attr('data-indice');
+
+    //QUAL É A RESPOSTA CERTA
+    var respCerta = perguntas [indice].correta;
 
 
 
+    //QUAL FOI A RESPOSTA QUE O USUARIO SELECIONOU
+    $('.resposta ').each(function () {
+        if ($(this).hasClass('selecionada')) {
+            var respostaEscolhida = $(this).attr('id');
+
+            if (respCerta == respostaEscolhida) {
+                alert('Aceeertou Miseraveeee!');
+            } else {
+                alert('Errrrroooooooou!');
+            } 
+        }
+    })
+});
